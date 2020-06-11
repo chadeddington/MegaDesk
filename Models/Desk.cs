98a380e5d3fcb6d45f00c3102fc718e4ae300652
Chad Eddington <cdeddington@outlook.com>
2020-06-11 13:40:26 -0600

    Add project files.

---
+++ Models/Desk.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MegaDesk.Models
+{
+    public class Desk
+    {
+        public int DeskId { get; set; }
+        public decimal Width { get; set; }
+        public decimal Depth { get; set; }
+        [Display(Name ="Number of Drawers")]
+        public int NumberOfDrawers { get; set; }
+        [Display(Name ="Desktop Material")]
+        public int DesktopMaterialId { get; set; }
+
+        /* navigation properties*/
+        public DesktopMaterial DesktopMaterial { get; set; }
+    }
+}
